7c0f380cd015fc40c5e24a82dd4a2d75a5cbf8f2
lirazanschool <christinemarie.lirazan@wheelermagnet.com>
2025-10-10 11:50:47 -0400

    Update Shapes.h
    Co-authored-by: Daremyrtle <167142814+Daremyrtle@users.noreply.github.com>

--- Shapes.h
+++ Shapes.h
@@ -39,7 +39,8 @@ public:
 	Parallelogram(double x1, double y1, double x2,
 		double y2, double x3, double y3,
 		double x4, double y4);
-	double getArea(int R);
+	double getArea();
+
 };
 
 class Rectangle : public Parallelogram {
